83e9ab5553cadb7e2f1af216bec64f99a5948627
hecyebesdelpino <61537808+hecyebesdelpino@users.noreply.github.com>
2023-05-31 14:12:52 +0200

    Add files via upload
    Nuevos estilos de botones y posiciones actualizado

--- ShinyAppCustomizedStyles.R
+++ ShinyAppCustomizedStyles.R
@@ -1,63 +1,123 @@
-#Changes the button style. Will apply when the button has been selected it would change color
-tags$head(
-  tags$style(HTML("
-      .boton-pulsado {
-        background-color: green !important;
-        color: white !important;
-      }"
-  )))
+# #Changes the button style. Will apply when the button has been selected it would change color
 
-tags$head(
-  tags$style(HTML("
-      .blue-button {
-        background-color: blue;
-        color: white;
-      }"
-  )))
+code_style <- "
+.boton-pulsado {
+  background-color: green !important;
+  color: white !important;
+}
 
-tags$head(
-  tags$style(HTML("
-      .left-btn {
-        float: left;
-      }
-      
-      .right-btn {
-        float: right;
-      }
-    ")))
+.blue-button {
+  background-color: blue;
+  color: white;
+}
 
-tags$head(
-  tags$style(
-    HTML("
-      .table-style {
-        /* Estilos para la tabla */
-      }
-      .mi-tabla th {
-        /* Estilos para las celdas del encabezado */
-      }
-      .mi-tabla td {
-        /* Estilos para las celdas de datos */
-      }
-    ")
-  )
-)
 
-tags$head(
-  tags$style(
-    HTML("
-      .mi-columna td:first-child {
-        background-color: #cfe2f3; /* Fondo gris azulado */
-        font-weight: bold; /* Letras en negrita */
-      }
-    ")
-  )
-)
+.light-blue-button {
+  background-color: darkblue;
+  color: white;
+}
 
-tags$head(
-  tags$script("
-      $(document).on('change', '.decimal-input', function() {
-        var value = $(this).val().replace('.', ',');
-        $(this).val(value);
-      });
-    ")
-)
+.green-button {
+  background-color: darkgreen;
+  color: white;
+}
+
+.left-btn {
+  float: left;
+}
+
+.right-btn {
+  float: right;
+}
+
+.table-style {
+  /* Estilos para la tabla */
+}
+
+.mi-tabla th {
+  /* Estilos para las celdas del encabezado */
+}
+
+.mi-tabla td {
+  /* Estilos para las celdas de datos */
+}
+
+.mi-columna td:first-child {
+  background-color: #cfe2f3; /* Fondo gris azulado */
+  font-weight: bold; /* Letras en negrita */
+}
+"
+
+code_script <- "
+$(document).on('change', '.decimal-input', function() {
+  var value = $(this).val().replace('.', ',');
+  $(this).val(value);
+});
+"
+
+
+# tags$head(
+#   tags$style(HTML("
+#       .boton-pulsado {
+#         background-color: green !important;
+#         color: white !important;
+#       }"
+#   )))
+# 
+# tags$head(
+#   tags$style(HTML("
+#       .blue-button {
+#         background-color: blue;
+#         color: white;
+#       }"
+#   )))
+# 
+# tags$head(
+#   tags$style(HTML("
+#       .left-btn {
+#         float: left;
+#       }
+#       
+#       .right-btn {
+#         float: right;
+#       }
+#     ")))
+# 
+# tags$head(
+#   tags$style(
+#     HTML("
+#       .table-style {
+#         /* Estilos para la tabla */
+#       }
+#       .mi-tabla th {
+#         /* Estilos para las celdas del encabezado */
+#       }
+#       .mi-tabla td {
+#         /* Estilos para las celdas de datos */
+#       }
+#     ")
+#   )
+# )
+# 
+# tags$head(
+#   tags$style(
+#     HTML("
+#       .mi-columna td:first-child {
+#         background-color: #cfe2f3; /* Fondo gris azulado */
+#         font-weight: bold; /* Letras en negrita */
+#       }
+#     ")
+#   )
+# )
+# 
+# tags$head(
+#   tags$script("
+#       $(document).on('change', '.decimal-input', function() {
+#         var value = $(this).val().replace('.', ',');
+#         $(this).val(value);
+#       });
+#     ")
+# )
+
+
+
